4aae676a022700d65bfcf74c0cf2ad6f23d9d09f
Your Name <you@example.com>
2026-04-09 22:36:33 -0500

    Create career

--- traveller_character_generator.go
+++ traveller_character_generator.go
@@ -81,6 +81,7 @@ func read_input() string {
 
 func main() {
 	char := Character{age: 18}
+	var career Career
 
 	// Prompt the user to enter their name.
 	fmt.Print("Please enter your name: ")
@@ -95,7 +96,7 @@ func main() {
 
 	fmt.Printf("Name: %s\n", char.name)
 	fmt.Printf("Age: %d\n", char.age)
-	fmt.Printf("Stat: %d\n", char.stat)
+	fmt.Printf("Stat: %+v\n", char.stat)
 
 	for {
 		fmt.Print("What service? 1:Navy, 2:Marines, 3:Army, 4:Scouts, 4:Merchants, 6:Other (1-6): ")
@@ -104,6 +105,33 @@ func main() {
 		if !(service_choice == "1" || service_choice == "2" || service_choice == "3" || service_choice == "4" || service_choice == "5" || service_choice == "6") {
 			continue
 		}
+		switch service_choice {
+		case "1":
+			char.service = "Navy"
+			career = Career{name: "Navy", enlistment: 8, draft: 1, survival: 5, commission: 10, promotion: 8, reenlist: 6}
+		case "2":
+			char.service = "Marines"
+			career = Career{name: "Marines", enlistment: 9, draft: 2, survival: 6, commission: 9, promotion: 9, reenlist: 6}
+		case "3":
+			char.service = "Army"
+			career = Career{name: "Army", enlistment: 5, draft: 3, survival: 5, commission: 5, promotion: 6, reenlist: 7}
+		case "4":
+			char.service = "Scouts"
+			career = Career{name: "Scouts", enlistment: 7, draft: 4, survival: 7, commission: 0, promotion: 0, reenlist: 3}
+		case "5":
+			char.service = "Merchants"
+			career = Career{name: "Merchants", enlistment: 7, draft: 5, survival: 5, commission: 4, promotion: 10, reenlist: 4}
+		case "6":
+			char.service = "Other"
+			career = Career{name: "Other", enlistment: 3, draft: 6, survival: 5, commission: 0, promotion: 0, reenlist: 5}
+		}
 		break
 	}
+
+	fmt.Printf("Name: %s\n", char.name)
+	fmt.Printf("Age: %d\n", char.age)
+	fmt.Printf("Stat: %+v\n", char.stat)
+	fmt.Printf("Service: %s\n", char.service)
+	fmt.Printf("Career: %+v\n", career)
+
 }
